7cd2cc9b22814376c455f1b6d38f3fca38e5166e
racchu08 <rachanarachana610@gmail.com>
2022-09-18 19:04:12 +0530

    done

---
+++ configs/db.config.js
@@ -0,0 +1,4 @@
+module.exports =  {
+    DB_URL : "mongodb+srv://cluster0.c0bmc.mongodb.net/myFirstDatabase"
+    // mongosh "mongodb+srv://cluster0.c0bmc.mongodb.net/myFirstDatabase" --apiVersion 1 --username Kummi
+}
